2a021a2e297e352b03b0f7f6fe26046f176fb32e
Connie Chan <conniechan.10@gmail.com>
2019-03-07 01:04:15 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,3 +1,3 @@
 // Code your solution in this file!
 let companyName = 'Scuber';
-expect(function () { companyName = 'specialCompany' }).to.throw(TypeError);
+expect(function () { companyName = 'Scuber' }).to.throw(TypeError);
